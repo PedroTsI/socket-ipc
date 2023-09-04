5839cb903c17cca3f91accd57d25d33bcd921a76
Pedro Paulo Cantalice Marinho Braga <pedro.cantalice@assert.ifpb.edu.br>
2023-09-03 21:54:47 -0300

    Gugawag Activity

--- src/com/gugawag/so/ipc/NomeClient.java
+++ src/com/gugawag/so/ipc/NomeClient.java
@@ -28,7 +28,7 @@ public class NomeClient {
 
 			PrintWriter pout = new PrintWriter(servidorSock.getOutputStream(), true);
 			// TODO Altere abaixo para enviar seu nome ao servidor
-			pout.println("SEU NOME AQUI");
+			pout.println("Pedro Paulo Cantalice Marinho Braga");
 			servidorSock.close();
 		}
 		catch (IOException ioe) {
